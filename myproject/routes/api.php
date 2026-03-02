608a7d0fefbfc5666401623c1bbd4674db834c07
Jur1n0 <danis.juraj16@gmail.com>
2026-03-02 14:49:39 +0100

    cvičenie 3

--- myproject/routes/api.php
+++ myproject/routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookApiController;
+use App\Http\Controllers\BookRestController;
+use App\Http\Controllers\BookRpcController;
+use App\Http\Controllers\BookSacController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/rpc/books/{id}/borrow', [BookRpcController::class, 'borrowBook']);
+Route::post('/rpc/books/{id}/return', [BookRpcController::class, 'returnBook']);
+
+Route::get('/sac/books/{id}', BookSacController::class);
+
+Route::prefix('rest')->group(function () {
+    Route::get('/books', [BookRestController::class, 'index']);
+    Route::get('/books/{id}', [BookRestController::class, 'show']);
+    Route::post('/books', [BookRestController::class, 'store']);
+    Route::put('/books/{id}', [BookRestController::class, 'update']);
+    Route::delete('/books/{id}', [BookRestController::class, 'destroy']);
+    Route::get('/books/create', [BookRestController::class, 'create']);
+    Route::get('/books/{id}/edit', [BookRestController::class, 'edit']);
+});
+
+Route::prefix('restapi')->group(function () {
+    Route::get('/books', [BookRestController::class, 'index']);
+    Route::get('/books/{id}', [BookRestController::class, 'show']);
+    Route::post('/books', [BookRestController::class, 'store']);
+    Route::put('/books/{id}', [BookRestController::class, 'update']);
+    Route::delete('/books/{id}', [BookRestController::class, 'destroy']);
+});
